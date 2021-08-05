1c7d0d9770005cde218c34c33c1d54523b298024
jee ar <57131696+w33ing@users.noreply.github.com>
2021-08-05 09:22:22 +0800

    WordCounting

---
+++ WordCountingDictionary/WordCountingDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    private static readonly string Text = "Mary had a little lamb " + "little Lamb, little Lamb, " + "Mary had a Little lamb, " + "whose fleece were white as snow.";
+    public static void Main()
+    {
+        IDictionary<String, int> wordOccurrenceMap = GetWordOccurrenceMap(Text);
+        PrintWordOccurrenceCount(wordOccurrenceMap);
+        Console.Read();
+    }
+    private static IDictionary<string, int> GetWordOccurrenceMap(string text)
+    {
+        string[] tokens = text.Split(' ', '.', ',', '-', '?', '!');
+        IDictionary<string, int> words = new SortedDictionary<string, int>();
+        foreach (string word in tokens)
+        {
+            if (!string.IsNullOrEmpty(word.Trim()))
+            {
+                int count;
+                if (!words.TryGetValue(word, out count))
+                {
+                    count = 0;
+                }
+                words[word] = count + 1;
+            }
+        }
+        return words;
+    }
+    private static void PrintWordOccurrenceCount(IDictionary<string, int> wordOccurenceMap)
+    {
+        foreach (var wordEntry in wordOccurenceMap)
+        {
+            Console.WriteLine("Word '{0}' occurs {1} time(s) in the text", wordEntry.Key, wordEntry.Value);
+        }
+    }
+}
